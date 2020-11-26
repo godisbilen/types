dc07ff69cee3a73309a24cfe4374614154daa346
Albin Médoc <stamline203@gmail.com>
2020-11-26 16:58:03 +0100

    Added interfaces for point, polygon, order, car and region

---
+++ index.d.ts
@@ -0,0 +1,48 @@
+export interface point {
+    type: 'Point';
+    coordinates: [number, number];
+}
+
+export interface polygon {
+    type: 'Polygon';
+    coordinates: [Array<[number, number]>]
+}
+
+export interface create_order {
+    phone_number?: string;
+    email?: string;
+    region?: string;
+    location: point;
+    full_address?: string;
+    stop_time: number;
+    placed?: Date;
+    started?: Date;
+    arrival_time?: Date;
+    arrived?: Date;
+    completed?: Date;
+}
+
+export interface order extends create_order {
+    region: string;
+    full_address: string;
+    stop_time: number;
+    placed: Date;
+    arrival_time: Date;
+}
+
+export interface car {
+    _id?: string;
+    driver?: string;
+    region?: string;
+    start_time: number;
+    start_pos: point;
+    orders?: order[];
+}
+
+export interface region {
+    _id?: string;
+    name: string;
+    active: boolean;
+    bounds: polygon;
+    cars: string[];
+}
